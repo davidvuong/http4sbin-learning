0819741189d19c45220732de45025bcc26ccdf9a
David Vuong <david.vuong@cammy.com>
2016-10-28 14:39:12 +1100

    refactor+feat(http): compose services with orElse
    Also moved PingPongService out into a separate module, removed the
    RootService, and added a basic middleware example to log
    request/response to stdout.

--- src/main/scala/com/github/http4sbin/Boot.scala
+++ src/main/scala/com/github/http4sbin/Boot.scala
@@ -1,19 +1,29 @@
 package com.github.http4sbin
 
-import com.github.http4sbin.http.RootService
+import com.github.http4sbin.http.middleware.LoggingMiddleware
+import com.github.http4sbin.http.{PingPongService, ExampleService, UserAgentService}
+import org.http4s.HttpService
+import org.http4s.server.syntax._
 import org.http4s.server.ServerApp
 import org.http4s.server.blaze.BlazeBuilder
 
 import scala.util.Try
 
 object Boot extends ServerApp {
+  type Middleware = HttpService => HttpService
 
-  val service = RootService.service
+  val middlewareStack: Middleware = LoggingMiddleware(_)
+  val service = List(
+    PingPongService.service,
+    UserAgentService.service,
+    ExampleService.service
+  ).reduce((combined, service) => combined orElse service)
+  val serviceWithMiddleware = middlewareStack(service)
 
   def server(args: List[String]) = {
     BlazeBuilder
       .bindHttp(Try(System.getenv("PORT").toInt).toOption.getOrElse(8080), "0.0.0.0")
-      .mountService(service, "/")
+      .mountService(serviceWithMiddleware, "/")
       .start
   }
 }
--- src/main/scala/com/github/http4sbin/http/UserAgentService.scala
+++ src/main/scala/com/github/http4sbin/http/UserAgentService.scala
@@ -5,8 +5,9 @@ import org.http4s.headers.`User-Agent`
 import org.http4s.dsl._
 
 object UserAgentService {
+
   val service = HttpService {
-    case req @ GET -> Root => {
+    case req @ GET -> Root / "user-agent" => {
       req.headers.get(`User-Agent`) match {
         case Some(userAgent) => Ok(userAgent.toString)
         case None => NoContent()
