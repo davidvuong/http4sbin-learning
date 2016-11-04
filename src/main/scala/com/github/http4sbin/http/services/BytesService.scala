66620286b7001db588695db2f006bc4cdc702f15
David Vuong <david.vuong@cammy.com>
2016-11-04 19:23:54 +1100

    refactor(services): use streams and simplified try.catch

--- src/main/scala/com/github/http4sbin/http/services/BytesService.scala
+++ src/main/scala/com/github/http4sbin/http/services/BytesService.scala
@@ -1,39 +1,41 @@
 package com.github.http4sbin.http.services
 
-import scala.util.Random.nextBytes
-
+import scala.util.Random
 import org.http4s.HttpService
 import org.http4s.dsl._
+import argonaut._
+import Argonaut._
 
-import argonaut._, Argonaut._
+import scalaz._
+import scalaz.stream._
+import scalaz.concurrent.Task
 
 import com.github.http4sbin.http.ArgonautInstances._
 
 object BytesService {
 
   object RandomBytesInVar {
+    val MIN_RANDOM_N = 0
+    val MAX_RANDOM_N = 30
+
     def unapply(str: String): Option[Int] = {
-      try {
-        str.toInt match {
-          case n: Int if n < 1  => None
-          case n: Int if n > 30 => None
-          case n: Int => Some(n)
-        }
-      } catch {
-        case e: NumberFormatException => None
-      }
+      \/.fromTryCatchNonFatal(str.toInt).toOption.filter(n => n >= MIN_RANDOM_N || n <= MAX_RANDOM_N)
     }
   }
 
-  private def getRandomBytes(n: Integer): String = {
-    val randomBytes = new Array[Byte](n)
-    nextBytes(randomBytes)
-    randomBytes.foldLeft("")(_ + _.toChar)
+  private def getRandomBytes(n: Int): String = {
+    val randomBytes: Process[Task, Byte] = Process.repeatEval(
+      Task { (Random.nextInt(256) - 128).toByte }
+    )
+    randomBytes.take(100).runLog.run.foldLeft("")(_ + _.toChar)
   }
 
   val service = HttpService {
     case req @ GET -> Root / "bytes" / RandomBytesInVar(n) => {
-      Ok(Json("bytes" := getRandomBytes(n).asJson))
+      val randomBytes: Process[Task, Byte] = Process.repeatEval(
+        Task { (Random.nextInt(256) - 128).toByte }
+      )
+      Ok(Json("bytes" := getRandomBytes(n)))
     }
   }
 }
